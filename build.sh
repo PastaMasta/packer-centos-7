0ae61727a055522a647560442aebeda6232d7f26
PastaMasta <pasta.masta2902@gmail.com>
2017-05-22 21:18:50 +0100

    Added remote packer build script

--- build.sh
+++ build.sh
@@ -1,6 +1,27 @@
-#! /bin/bash -x
+#! /bin/bash
 
-# Work-around for Vagrant post-processor filling /tmp
-[[ -d /data/virt/fast_storage ]] && export TMPDIR=/data/virt/fast_storage
+#
+# Build packer on $1
+#
 
-[[ -x ~/bin/packer ]] && ~/bin/packer build ./centos-7.json
+function usage {
+  echo "$* build_host packer_file.json"
+  exit 1
+}
+[[ $# -lt 2 ]] && usage $0
+
+build_host=$1
+packer_file=`readlink -e $2`
+
+cmd="
+[[ ! -x /usr/local/bin/packer ]] && echo 'No packer dum dum!' && exit 1
+cd `dirname ${packer_file}`
+/usr/local/bin/packer validate ${packer_file} || exit 1
+/usr/local/bin/packer build ${packer_file}
+"
+
+if [[ ${build_host} == "localhost" ]] ; then
+  bash -c "${cmd}"
+else
+  ssh -X ${build_host} bash -c "${cmd}"
+fi
