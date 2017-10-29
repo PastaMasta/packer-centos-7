11c3ab5ed213c965e3d90acd7dd5d98870158261
PastaMasta <pasta.masta2902@gmail.com>
2017-10-29 15:23:22 +0000

    Now build-time ssh key actualy works

--- build.sh
+++ build.sh
@@ -20,9 +20,10 @@ set -x
 # Generate key for packer to use during build
 ssh-keygen -f id_rsa -P ''
 openssl rsa -in ~/.ssh/id_rsa -outform pem > id_rsa.pem
+chmod 600 ./id_rsa.pem
 sed -i -e "/NEW_SSH_KEY/ s%CHANGE_ME_FOR_NEW_SSH_KEY%`cat id_rsa.pub`%" ./httpdir/centos-7-ks.cfg
 
-
+# Check and builderize
 if /usr/local/bin/packer validate ${packer_file} ; then
   /usr/local/bin/packer build ${packer_file}
 else
